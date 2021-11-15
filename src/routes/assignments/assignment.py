fb5a5052b99a1d11dfa9dc3e4d61e8d16481e0a0
vijay <vijaybalajitheboss@gmail.com>
2021-11-15 23:12:56 +0530

    Setup create assignment endpoint

--- src/routes/assignments/assignment.py
+++ src/routes/assignments/assignment.py
@@ -20,13 +20,39 @@ def getAssignments():
         result = get_assignments_init(data)
         return result
     if request.method == "GET":
-        result = """<h3>POST Json data of user-type and CourseID to get assignments created</h3></br>
+        result = """<h3>POST Json data of CourseID to get assignments created</h3></br>
         <pre>
         JSON Data format:
         {
-            "type": "student/prof",
-            "UserID": "student/prof ID" <int>,
             "CourseID": "CSE1005"
+        }
+        </pre>
+        """
+        return result
+
+
+@assign_bp.route("/create_assignment", methods=["GET", "POST"])
+def createAssignment():
+    if request.method == "POST":
+        key = request.args.get("key")
+        if key == os.getenv("API_KEY"):
+            pass
+        else:
+            return "<h2>Invalid Key, To obtain access to the key contact Project Lead.</h2>"
+        data = request.get_json()
+        result = create_assignment(data)
+        return result
+    if request.method == "GET":
+        result = """<h3>POST Json data of CourseID, AssignmentName, AssignmentDescription,
+        AssignmentDueDate to create assignment</h3></br>
+        <pre>
+        JSON Data format example:
+        {
+            "CourseID": "CSE1005",
+            "AssignmentName": "Assignment1",
+            "AssignmentDescription": "Assignment1",
+            "AssignmentDueDate": "2020-01-01",
+            "AssignmentMaxScore": "100"
         }
         </pre>
         """
--- src/routes/assignments/assignment_functions.py
+++ src/routes/assignments/assignment_functions.py
@@ -12,3 +12,29 @@ def get_assignments_init(data):
         result["status"] = "false"
         result["message"] = "No assignments found for this course"
     return result
+
+
+def create_assignment(data):
+    given_CourseID = data["CourseID"]
+    with open("data/data.json") as json_file:
+        complete_data = json.load(json_file)
+    new_assignment = {}
+    if given_CourseID in complete_data["AssignmentData"]:
+        new_assignment = {
+            "AssignmentID": complete_data["AssignmentData"][given_CourseID][-1][
+                "AssignmentID"
+            ]
+            + 1,
+            "AssignmentName": data["AssignmentName"],
+            "AssignmentDescription": data["AssignmentDescription"],
+            "AssignmentDueDate": data["AssignmentDueDate"],
+            "AssignmentMaxScore": data["AssignmentMaxScore"],
+            "AssignmentCompleted": {},
+        }
+        complete_data["AssignmentData"][given_CourseID].append(new_assignment)
+    else:
+        complete_data["AssignmentData"][given_CourseID] = [new_assignment]
+
+    with open("data/data.json", "w") as json_file:
+        json.dump(complete_data, json_file, indent=4, separators=(",", ": "))
+    return {"status": "true", "message": "Assignment created successfully"}
