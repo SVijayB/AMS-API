bed8cd2e32bc5f5f85c690bcb3a44c43d6166603
vijay <vijaybalajitheboss@gmail.com>
2021-11-15 23:34:29 +0530

    Fixed assignment new assignment not being created

--- src/routes/assignments/assignment_functions.py
+++ src/routes/assignments/assignment_functions.py
@@ -18,19 +18,22 @@ def create_assignment(data):
     given_CourseID = data["CourseID"]
     with open("data/data.json") as json_file:
         complete_data = json.load(json_file)
-    new_assignment = {}
+    AssigmentID = 1
+    try:
+        AssigmentID = (
+            complete_data["AssignmentData"][given_CourseID][-1]["AssignmentID"] + 1
+        )
+    except:
+        pass
+    new_assignment = {
+        "AssignmentID": AssigmentID,
+        "AssignmentName": data["AssignmentName"],
+        "AssignmentDescription": data["AssignmentDescription"],
+        "AssignmentDueDate": data["AssignmentDueDate"],
+        "AssignmentMaxScore": data["AssignmentMaxScore"],
+        "AssignmentCompleted": {},
+    }
     if given_CourseID in complete_data["AssignmentData"]:
-        new_assignment = {
-            "AssignmentID": complete_data["AssignmentData"][given_CourseID][-1][
-                "AssignmentID"
-            ]
-            + 1,
-            "AssignmentName": data["AssignmentName"],
-            "AssignmentDescription": data["AssignmentDescription"],
-            "AssignmentDueDate": data["AssignmentDueDate"],
-            "AssignmentMaxScore": data["AssignmentMaxScore"],
-            "AssignmentCompleted": {},
-        }
         complete_data["AssignmentData"][given_CourseID].append(new_assignment)
     else:
         complete_data["AssignmentData"][given_CourseID] = [new_assignment]
